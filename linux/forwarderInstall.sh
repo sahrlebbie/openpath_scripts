09cacf258c9f66118ffb4fe07960910c084ba104
Sahr Lebbie <sahr.lebbie@openpathus.com>
2021-10-14 14:26:01 -0400

    Update forwarderInstall.sh
    Modified to chown at the end instead of sudo -u splunk

--- linux/forwarderInstall.sh
+++ linux/forwarderInstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Set the Deployment Server IP
-DEPLOYMENTSERVER_IP=" "
+DEPLOYMENTSERVER_IP=""
 PASSWORD=""
 APPS_DS_APP="" Name of the app to connect to Deployment Server. This will create it locally and then it can be overridden at the Deploymentserver level.
 #Only uncomment this HOSTNAME variable you have  a special name you would like to give your forwarders upon install.
-#HOSTNAME="forwarder$RANDOM"
+HOSTNAME=""
 
 #Adds the Splunk User if not added. You can sett he password later through automation tool or a script.
 sudo useradd splunk
@@ -25,7 +25,6 @@ sudo tar -xvf /tmp/splunkforwarder-8.2.1-ddff1c41e5cf-Linux-x86_64.tgz -C /opt
 sudo chown -R splunk:splunk /opt/splunkforwarder/
 sudo -u splunk /opt/splunkforwarder/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd $PASSWORD
 
-#sudo -u splunk /opt/splunkforwarder/bin/splunk enable listen 9997
 #Now let's make sure we enter the correct server name and hostname reporting for Splunk in the UI.
 
 
@@ -34,18 +33,19 @@ sudo -u splunk /opt/splunkforwarder/bin/splunk start --accept-license --answer-y
 #Next we will set the default servername
 #Finally we enable Splunk Web SSL. Be sure to custom set your certs as this script does not do that.
 sudo /opt/splunkforwarder/bin/splunk enable boot-start -user splunk
-sudo -u splunk mkdir /opt/splunkforwarder/etc/apps/$APPS_DS_APP
-sudo -u splunk mkdir /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local
-sudo -u splunk touch /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local/deploymentclient.conf
+sudo mkdir /opt/splunkforwarder/etc/apps/$APPS_DS_APP
+sudo mkdir /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local
+sudo touch /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local/deploymentclient.conf
 
 #Uncomment the lines below to set the host inputs and servername as refected on the DS.
-#sudo -u splunk /opt/splunk/bin/splunk set default-hostname $HOSTNAME -auth admin:Splunk18!
-#sudo -u splunk /opt/splunk/bin/splunk set servername $HOSTNAME -auth admin:Splunk18!
+sudo /opt/splunk/bin/splunk set default-hostname $HOSTNAME -auth admin:$PASSSWORD
+sudo /opt/splunk/bin/splunk set servername $HOSTNAME -auth admin:$PASSWORD
 
-#sudo -u splunk /opt/splunk/bin/splunk set deploy-poll $DEPLOYMENTSERVER_IP:8089 -auth admin:Splunk18!
+sudo /opt/splunk/bin/splunk set deploy-poll $DEPLOYMENTSERVER_IP:8089 -auth admin:$PASSWORD
 #Let's put in our target Deploymentserver
-sudo -u splunk echo "[deployment-client]" > /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local/deploymentclient.conf
+sudo  echo "[deployment-client]" > /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local/deploymentclient.conf
 
-sudo -u splunk echo "[target-broker:deploymentServer]" >> /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local/deploymentclient.conf
-sudo -u splunk echo "targetUri= $DEPLOYMENTSERVER_IP:8089" >> /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local/deploymentclient.conf
+sudo echo "[target-broker:deploymentServer]" >> /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local/deploymentclient.conf
+sudo echo "targetUri= $DEPLOYMENTSERVER_IP:8089" >> /opt/splunkforwarder/etc/apps/$APPS_DS_APP/local/deploymentclient.conf
+sudo chown -R splunk:splunk /opt/splunkforwarder/
 sudo -u splunk /opt/splunkforwarder/bin/splunk restart
